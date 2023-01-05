aeac8c757cd3c635b843c85bb51ecf357b60d8da
Thyago de Oliveira Freitas <thyago.olifreitas@gmail.com>
2023-01-05 15:03:37 -0300

    Modificando as classes Iphone, Nokia e Smartphone e incluindo testes na classe principal

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Smartphone Nokia: ");
+Smartphone nokia = new Nokia(numero: "83912345678", modelo: "Lumia", imei: "0000000", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("YouTube");
+
+Console.WriteLine();
+
+Console.WriteLine("Smartphone Iphone: ");
+Smartphone iphone = new Iphone(numero: "83909871234", modelo: "12", imei: "9999999", memoria: 128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Linkedin");
